a94fd6f36afc4e74ebe5cf5878e245e81a68feff
Daniel González Briseño <qbixmex@gmail.com>
2025-01-14 18:57:00 -0800

    Documentation Swagger
    - Added to Users
    - Added to Breeds
    - Added to Cats
    - Added to Auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import Role from '../common/enums/role.enum';
 import Auth from '../auth/decorators/auth.decorator';
 
+@ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
